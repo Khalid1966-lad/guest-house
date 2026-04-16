d84d4416575de1052092bfb52ccaefa7029e9b31
Z User <z@container>
2026-04-16 16:41:45 +0000

    v2.2.2: trigger rebuild - DB now has code column

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,7 +1,7 @@
 // This file is auto-updated by the version script
 // Do not edit manually - use: bun run version <version>
 
-export const APP_VERSION = "v2.2.1"
+export const APP_VERSION = "v2.2.2"
 export const VERSION_DATE = "2026-07-04"
 export const VERSION_NAME = "Code maison d'hôtes (GH001...) dans numéros de facture, fix contrainte unique"
 export const COPYRIGHT_YEAR = 2026
